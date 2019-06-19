b28925d33726d2dade64538ad982aa8ecb59ca9b
kaiquefernandes <kaique_miyasato@hotmail.com>
2019-06-19 01:18:14 -0300

    botão excluir adicionado no criar conta

--- ProjFinanceiro/Controllers/ContaController.cs
+++ ProjFinanceiro/Controllers/ContaController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ProjFinanceiro.Models;
 
 namespace ProjFinanceiro.Controllers
 {
@@ -10,7 +12,17 @@ namespace ProjFinanceiro.Controllers
     {
         public IActionResult Index()
         {
+            ContaModel conta = new ContaModel();
+            ViewBag.ListaConta = conta.ListaConta();
             return View();
         }
+
+        IHttpContextAccessor HttpContextAccessor;
+        public ContaController(IHttpContextAccessor httpContextAccessor)
+        {
+            HttpContextAccessor = httpContextAccessor;
+        }
+
     }
+
 }
---
+++ ProjFinanceiro/Models/ContaModel.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using ProjFinanceiro.util;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjFinanceiro.Models
+{
+    public class ContaModel
+    {
+        public int Id {get; set;}
+        public string Nome { get; set; }
+        public double Saldo { get; set; }
+        public int usuario_id { get; set; }
+        IHttpContextAccessor HttpContextAccessor;
+
+
+        public ContaModel() {
+
+        }
+
+        public ContaModel(IHttpContextAccessor httpContextAccessor) {
+            HttpContextAccessor = httpContextAccessor;
+        }
+
+        public List<ContaModel> ListaConta()
+        {
+            List<ContaModel> lista = new List<ContaModel>();
+            ContaModel item;
+            string id_usuario_logado = HttpContextAccessor.HttpContext.Session.GetString("IdUsuarioLogado");
+            string sql = $"SELECT ID, NOME, SALDO, USUARIO_ID FROM CONTA WHERE USUARIO_ID = {id_usuario_logado}";
+            Dal objDal = new Dal();
+            DataTable dt = objDal.RetDataTable(sql);
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                item = new ContaModel();
+                item.Id = int.Parse(dt.Rows[i]["ID"].ToString());
+                item.Nome = dt.Rows[i]["NOME"].ToString();
+                item.Saldo = Double.Parse(dt.Rows[i]["SALDO"].ToString());
+                item.usuario_id = int.Parse(dt.Rows[i]["USUARIO_ID"].ToString());
+                lista.Add(item);
+
+            }
+            return lista;
+        }
+    }
+}
